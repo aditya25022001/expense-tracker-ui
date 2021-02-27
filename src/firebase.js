5ccef1cdcfa194fc21f06b7b42c41c7e5f60487c
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-02-27 10:57:04 +0530

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfeCKs5Vt1amrU_xt0nqOHcXBHpnLHpGQ",
+    authDomain: "todoapp-ca23f.firebaseapp.com",
+    databaseURL: "https://todoapp-ca23f-default-rtdb.firebaseio.com",
+    projectId: "todoapp-ca23f",
+    storageBucket: "todoapp-ca23f.appspot.com",
+    messagingSenderId: "429722728213",
+    appId: "1:429722728213:web:c5cffdc4dfe03b1b929b1c",
+    measurementId: "G-4FG75T5C07"
+  };
